d3dba5a0501cb60eb327e982137e6d11b11d8d7b
Nadir Fejzic <nadirfejzo@gmail.com>
2023-10-29 15:02:10 +0100

    fix: don't check for longer keyword after whitespace

--- src/lexer/next_impl.rs
+++ src/lexer/next_impl.rs
@@ -15,6 +15,12 @@ macro_rules! next_impl {
         if let Some((found_token, cursor)) = $self.$fallible_fn(curr - $self.curr + 1) {
             token = Some(found_token);
             curr = cursor;
+
+            // token can't contain a whitespace, so this token has the maximum length already
+            if $self.src.get(cursor).map(|s| s.is_whitespace()).unwrap_or(false) {
+                $self.curr = curr;
+                return token;
+            }
         }
         )*
 
