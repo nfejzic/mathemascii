a2d52c8bc3f9169534c9d832c8550418134b6553
Nadir Fejzic <nadirfejzo@gmail.com>
2023-10-29 23:55:16 +0100

    test: add back accidentally deleted tests

--- src/lexer/tests/greeks.rs
+++ src/lexer/tests/greeks.rs
@@ -1,1 +1,54 @@
+use crate::lexer::keywords::greek::Greek;
 
+use super::super::*;
+
+#[test]
+fn greek_pi() {
+    let src = "pi";
+    let mut tokens = src.tokenize();
+    assert_eq!(
+        tokens.next(),
+        Some(Token::new("pi", TokenKind::Greek(Greek::Pi)))
+    );
+
+    let src = "Pi";
+    let mut tokens = src.tokenize();
+    assert_eq!(
+        tokens.next(),
+        Some(Token::new("Pi", TokenKind::Greek(Greek::BigPi)))
+    );
+}
+
+#[test]
+fn greek_alpha() {
+    let src = "alpha";
+    let mut tokens = src.tokenize();
+    assert_eq!(
+        tokens.next(),
+        Some(Token::new("alpha", TokenKind::Greek(Greek::Alpha)))
+    );
+}
+
+#[test]
+fn too_long() {
+    let src = "abcdefghijklmnopqrstuvwxy";
+    let mut tokens = src.tokenize();
+    assert_eq!(tokens.next(), None);
+}
+
+#[test]
+fn multiple() {
+    let src = "alphabetagammadelta";
+
+    let tokens: Vec<_> = src.tokenize().collect();
+
+    assert_eq!(
+        tokens,
+        vec![
+            Token::new("alpha", TokenKind::Greek(Greek::Alpha)),
+            Token::new("beta", TokenKind::Greek(Greek::Beta)),
+            Token::new("gamma", TokenKind::Greek(Greek::Gamma)),
+            Token::new("delta", TokenKind::Greek(Greek::Delta)),
+        ]
+    );
+}
--- src/lexer/tests/numbers.rs
+++ src/lexer/tests/numbers.rs
@@ -1,1 +1,40 @@
+use super::super::*;
 
+#[test]
+fn only_number() {
+    let src = "24.42";
+    let mut tokens = src.tokenize();
+    assert_eq!(tokens.next(), Some(Token::new("24.42", TokenKind::Number)));
+}
+
+#[test]
+fn start_with_dot() {
+    let src = ".42";
+    let mut tokens = src.tokenize();
+    assert_eq!(tokens.next(), Some(Token::new(".42", TokenKind::Number)));
+
+    let src = ".42a";
+    let mut tokens = src.tokenize();
+    assert_eq!(tokens.next(), Some(Token::new(".42", TokenKind::Number)));
+}
+
+#[test]
+fn start_and_stop_dot() {
+    let src = ".42.3";
+    let mut tokens = src.tokenize();
+    assert_eq!(tokens.next(), Some(Token::new(".42", TokenKind::Number)));
+}
+
+#[test]
+fn stop_at_non_digit() {
+    let src = "24.42a";
+    let mut tokens = src.tokenize();
+    assert_eq!(tokens.next(), Some(Token::new("24.42", TokenKind::Number)));
+}
+
+#[test]
+fn stop_at_dot() {
+    let src = "24.42.";
+    let mut tokens = src.tokenize();
+    assert_eq!(tokens.next(), Some(Token::new("24.42", TokenKind::Number)));
+}
