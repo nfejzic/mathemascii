7ab1c73db5f9314b84b99acbabd55e27785f6389
Nadir Fejzic <nadirfejzo@gmail.com>
2023-11-05 18:36:07 +0100

    test: add snap tests for obrace and ubrace

--- src/parser/tests/binary.rs
+++ src/parser/tests/binary.rs
@@ -1,3 +1,5 @@
 use super::Snapshot;
 
 super::test_snap!(frac, "frac 1.44 2 frac(x)(3.14)");
+super::test_snap!(ubrace, "ubrace(1+2+3+4)_(\"4 terms\")");
+super::test_snap!(obrace, "obrace(1+2+3+4)_(\"4 terms\")");
