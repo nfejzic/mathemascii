0ad46d622ce1f98ee5324095b98dcf0e8e3e4539
Nadir Fejzic <nadirfejzo@gmail.com>
2023-11-05 19:48:05 +0100

    test: add snap test for wrong order of sub and superscripts

--- src/parser/tests/sub_sup_scripts.rs
+++ src/parser/tests/sub_sup_scripts.rs
@@ -3,3 +3,5 @@ use super::Snapshot;
 super::test_snap!(n_to_infty, "n->oo");
 super::test_snap!(n_to_infty_grouping, "(n->oo)");
 super::test_snap!(complex_subscripts, "lim_(N->oo) sum_(i=0)^N");
+
+super::test_snap!(wrong_order, "sum^N_(N->oo)");
